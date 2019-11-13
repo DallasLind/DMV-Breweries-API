a4993ec896174d1f46862fed141572b15a38f7d9
DallasLind <mcgraw.dallas@gmail.com>
2019-11-13 10:15:12 -0500

    Attempting seed

--- db/seed.js
+++ db/seed.js
@@ -2,3 +2,19 @@ const Brewery = require('../models/Breweries');
 const fetch = require("node-fetch");
 
 const urls = ["https://api.openbrewerydb.org/breweries?by_state=maryland", "https://api.openbrewerydb.org/breweries?by_state=district_of_columbia", "https://api.openbrewerydb.org/breweries?by_state=virginia"]
+
+fetch(["https://api.openbrewerydb.org/breweries?by_state=maryland", "https://api.openbrewerydb.org/breweries?by_state=district_of_columbia", "https://api.openbrewerydb.org/breweries?by_state=virginia"], {
+  headers: {
+    Accept: 'application/json'
+  }
+})
+  .then(res => res.json())
+  .then(res => {
+    Brewery.remove({}).then(() => {
+      Brewery.create(res.results).then(Breweries => {
+        console.log(Breweries);
+        process.exit();
+      });
+    });
+  })
+  .catch(err => console.log('error', err))

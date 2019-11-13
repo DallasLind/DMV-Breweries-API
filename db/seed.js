ddf845d78c4f11c5f65e56095d8bff5c70214020
DallasLind <mcgraw.dallas@gmail.com>
2019-11-13 12:05:36 -0500

    Made a lot of changes to index and seed

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,31 @@
-const Brewery = require('../models/Breweries');
-const fetch = require("node-fetch");
+const breweriesJSON = require("../models/breweries.json");
+const Breweries = require("../models/breweries");
 
-const urls = ["https://api.openbrewerydb.org/breweries?by_state=maryland", "https://api.openbrewerydb.org/breweries?by_state=district_of_columbia", "https://api.openbrewerydb.org/breweries?by_state=virginia"]
-
-fetch(["https://api.openbrewerydb.org/breweries?by_state=maryland", "https://api.openbrewerydb.org/breweries?by_state=district_of_columbia", "https://api.openbrewerydb.org/breweries?by_state=virginia"], {
-  headers: {
-    Accept: 'application/json'
-  }
+const breweriesData = breweriesJSON.map(item => {
+    const breweries = {}
+    breweries.name = item.name
+    breweries.breweryType = item.breweryType
+    breweries.street = item.street
+    breweries.city = item.city
+    breweries.state = item.state
+    breweries.postalCode = item.postalCode
+    breweries.country = item.country
+    breweries.longitude = item.longitude
+    breweries.latitude = item.latitude
+    breweries.phone = item.phone
+    breweries.websiteUrl = item.websiteUrl
+    return breweries
 })
-  .then(res => res.json())
-  .then(res => {
-    Brewery.remove({}).then(() => {
-      Brewery.create(res.results).then(Breweries => {
-        console.log(Breweries);
-        process.exit();
-      });
-    });
-  })
-  .catch(err => console.log('error', err))
+
+Breweries.deleteMany({})
+    .then(() => {
+        breweries.create(breweriesData)
+            .then(breweries => {
+                console.log(breweries)
+                process.exit();
+            })
+            .catch(err => {
+                console.log(err)
+                process.exit();
+            })
+    })
--- index.js
+++ index.js
@@ -1,34 +1,101 @@
-const app = require('express')();
-const parser = require('body-parser');
-const Breweries = require('./db/breweries');
+const app = express();
+const express = require("express");
+const mongoose = require("mongoose")
+const parser = require("body-parser");
+const Breweries = require("./models/breweries");
 
 app.use(parser.json());
 
+app.post("/breweries", function (req, res){
+    Breweries.create(req.body).then(breweries => {
+        res.json(breweries);
+    })
+})
 
-app.get('/', (req, res) => {
-  Breweries.find({}).then(BreweriesCollection => {
-    res.json(BreweriesCollection);
+app.get("/breweries", function (req, res) {
+    Breweries.find({}).then(breweries => {
+        res.json(breweries);
+    })
+})
+
+app.get("/breweries/name/:name", function(req, res) {
+    Breweries.find({ brand: req.params.brand }).then(breweries => {
+    res.json(breweries);
+    });
+});
+
+app.get("/breweries/breweryType/:breweryType", function(req, res) {
+    Breweries.find({ product: req.params.product }).then(breweries => {
+    res.json(breweries);
+    });
+});
+
+app.get("/breweries/street/:street", function(req, res) {
+    Breweries.find({ hex: req.params.hex }).then(breweries => {
+    res.json(breweries);
+    });
+});
+
+app.get("/breweries/city/:city", function(req, res) {
+    Breweries.find({ group: req.params.group }).then(breweries => {
+    res.json(breweries);
+    });
+});
+
+app.get("/breweries/state/:state", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
   });
 });
 
-app.post('/', (req, res) => {
-  Breweries.create(req.body).then(newBreweries => {
-    res.json(newBreweries);
+app.get("/breweries/postalCode/:postalCode", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
   });
 });
 
-app.put('/:id', (req, res) => {
-  Breweries.findOneAndUpdate({ id: req.params.id }, req.body, { new: true }).then(updatedBreweries => {
-    res.json(updatedBreweries);
+app.get("/breweries/country/:country", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
   });
 });
 
-app.delete('/:id', (req, res) => {
-  Breweries.findOneAndDelete({ id: req.params.id }).then(deletedBreweries => {
-    res.json(deletedBreweries);
+app.get("/breweries/longitude/:longitude", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
   });
 });
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000...');
+app.get("/breweries/latitude/:latitude", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
+  });
 });
+
+app.get("/breweries/phone/:phone", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
+  });
+});
+
+app.get("/breweries/websiteUrl/:websiteUrl", function(req, res) {
+  Breweries.find({ group: req.params.group }).then(breweries => {
+  res.json(breweries);
+  });
+});
+
+app.get('/:name', (req, res) => {
+  Breweries.find({ name: req.params.name }).then(result => {
+    res.json(breweries);
+  });
+});
+
+app.delete("/breweries/:id", function(req, res) {
+    Breweries.findOneAndDelete({_id: req.body.id}).then(breweries => {
+        res.json(breweries);
+    })
+})
+
+app.listen(4000, () =>
+    console.log("Yay breweries!!")
+)

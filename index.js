e95c3b18f5a4547b8d3b65930d82b4cfef23462e
DallasLind <mcgraw.dallas@gmail.com>
2019-11-13 12:23:29 -0500

    Would help if my paramaters were right

--- index.js
+++ index.js
@@ -9,92 +9,86 @@ app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
 
 
-app.post("/breweries", function (req, res){
-    Breweries.create(req.body).then(breweries => {
-        res.json(breweries);
-    })
-})
-
 app.get("/", function (req, res) {
     Breweries.find({}).then(breweries => {
-        res.json(breweries);
+        res.redirect("http://localhost:4000/breweries");
     })
 })
 
 app.get("/breweries/name/:name", function(req, res) {
-    Breweries.find({ brand: req.params.brand }).then(breweries => {
+    Breweries.find({ name: req.params.name}).then(breweries => {
     res.json(breweries);
     });
 });
 
 app.get("/breweries/breweryType/:breweryType", function(req, res) {
-    Breweries.find({ product: req.params.product }).then(breweries => {
+    Breweries.find({ breweryType: req.params.breweryType }).then(breweries => {
     res.json(breweries);
     });
 });
 
 app.get("/breweries/street/:street", function(req, res) {
-    Breweries.find({ hex: req.params.hex }).then(breweries => {
+    Breweries.find({ street: req.params.street }).then(breweries => {
     res.json(breweries);
     });
 });
 
 app.get("/breweries/city/:city", function(req, res) {
-    Breweries.find({ group: req.params.group }).then(breweries => {
+    Breweries.find({ city: req.params.city }).then(breweries => {
     res.json(breweries);
     });
 });
 
 app.get("/breweries/state/:state", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ state: req.params.state }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/postalCode/:postalCode", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ postalCode: req.params.postalCode }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/country/:country", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ country: req.params.country }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/longitude/:longitude", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ longitude: req.params.longitude }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/latitude/:latitude", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ latitude: req.params.latitude }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/phone/:phone", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ phone: req.params.phone }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get("/breweries/websiteUrl/:websiteUrl", function(req, res) {
-  Breweries.find({ group: req.params.group }).then(breweries => {
+  Breweries.find({ websiteUrl: req.params.websiteUrl }).then(breweries => {
   res.json(breweries);
   });
 });
 
 app.get('/:name', (req, res) => {
-  Breweries.find({ name: req.params.name }).then(result => {
-    res.json(result);
+  Breweries.find({ name: req.params.name }).then(breweries => {
+    res.json(breweries);
   });
 });
 
-app.delete("/breweries/:id", function(req, res) {
-    Breweries.findOneAndDelete({_id: req.body.id}).then(breweries => {
+app.delete("/breweries/:name", function(req, res) {
+    Breweries.findOneAndDelete({ name: req.body.name }).then(breweries => {
         res.json(breweries);
     })
 })

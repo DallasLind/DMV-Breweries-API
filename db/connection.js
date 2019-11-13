aebaa4ab1df467fe1223e687a25fb794dcf5a8da
DallasLind <mcgraw.dallas@gmail.com>
2019-11-13 11:38:11 -0500

    Fixing index and connection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 //Gotta figure out connect
-mongoose.connect('mongodb://localhost/', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/breweries', { useNewUrlParser: true })
+.then(() => {
+    console.log("Connected to the brewery db!")
+})
 
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const express = require("express");
-const app = express();
+const app = require('express')();
+const parser = require('body-parser');
+const Breweries = require('./db/breweries');
+
+app.use(parser.json());
+
+
+app.get('/', (req, res) => {
+  Breweries.find({}).then(BreweriesCollection => {
+    res.json(BreweriesCollection);
+  });
+});
+
+app.post('/', (req, res) => {
+  Breweries.create(req.body).then(newBreweries => {
+    res.json(newBreweries);
+  });
+});
+
+app.put('/:id', (req, res) => {
+  Breweries.findOneAndUpdate({ id: req.params.id }, req.body, { new: true }).then(updatedBreweries => {
+    res.json(updatedBreweries);
+  });
+});
+
+app.delete('/:id', (req, res) => {
+  Breweries.findOneAndDelete({ id: req.params.id }).then(deletedBreweries => {
+    res.json(deletedBreweries);
+  });
+});
 
 app.listen(4000, () => {
-  console.log("app listening on port 4000");
+  console.log('Listening on port 4000...');
 });
